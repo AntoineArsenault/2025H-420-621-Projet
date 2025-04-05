8b36157b980507d18646b6c7506fb91bdbe5ae5d
AntoineArsenault <e2258830@cmaisonneuve.qc.ca>
2025-04-05 14:08:28 -0400

    initialisation du chessboard

--- app/backend/game/board.py
+++ app/backend/game/board.py
@@ -1,8 +1,17 @@
 class Board:
     def __init__(self):
-            self.board = self.create_initial_board()  
+        self.board = self.create_initial_board() # Crée le plateau avec les pièces initiales
 
     def create_initial_board(self):
-        """ Créer un plateau de 8x8 avec les positions intitiales """
-        initial_board = []
+        """ Créer un plateau de 8x8 avec les positions initiales des pièces """
+        initial_board = [
+            ["rook-b", "knight-b", "bishop-b", "queen-b", "king-b", "bishop-b", "knight-b", "rook-b"],
+            ["pawn-b", "pawn-b", "pawn-b", "pawn-b", "pawn-b", "pawn-b", "pawn-b", "pawn-b"],
+            [None, None, None, None, None, None, None, None],
+            [None, None, None, None, None, None, None, None],
+            [None, None, None, None, None, None, None, None],
+            [None, None, None, None, None, None, None, None],
+            ["pawn-w", "pawn-w", "pawn-w", "pawn-w", "pawn-w", "pawn-w", "pawn-w", "pawn-w"],
+            ["rook-w", "knight-w", "bishop-w", "queen-w", "king-w", "bishop-w", "knight-w", "rook-w"]
+        ]
         return initial_board
