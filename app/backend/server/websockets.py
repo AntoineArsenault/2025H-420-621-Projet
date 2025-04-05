f55f35c7c7648d3d30905e418a79892df020c0ba
AntoineArsenault <e2258830@cmaisonneuve.qc.ca>
2025-04-05 14:13:11 -0400

    Ajout l'envoi de l'état du plateau

--- app/backend/server/websockets.py
+++ app/backend/server/websockets.py
@@ -1,3 +1,13 @@
+from flask_socketio import emit
+from game.game import Game
+game = Game()
+
+# Fonction d'enregistrement des événements
 def register_websocket_events(socketio):
-    print("Registering websocket events")
+    """Enregistre les événements de WebSocket."""
     
+    # Lorsqu'un client demande l'état du plateau
+    @socketio.on('get_board')
+    def handle_get_board():
+        print("Envoi du plateau...")
+        emit('update_board', game.get_board())  # Envoie l'état du plateau au frontend
